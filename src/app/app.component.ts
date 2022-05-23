a0a9c0bbb4d152dac763d0000ca5c531c8b86e1b
Christophe Jollivet <jollivet@apside.fr>
2022-05-23 16:20:18 +0200

    introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Form, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'crm-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularCRM';
+  fruit='apple';
+  myCssClass='apple';
+  names=['Bob', 'John', 'Paul']
+  car:string|undefined='renault'
+
+  validateForm(carForm:NgForm){
+    console.log(this.car);
+    console.log(carForm.value);
+
+  }
+
+  change($event:MouseEvent):void{
+    if(this.fruit==='apple'){
+      this.fruit='banana';
+      this.myCssClass='banana';
+    }else{
+      this.fruit='apple';
+      this.myCssClass='apple';
+    }
+    console.log($event);
+  }
 }
