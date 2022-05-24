6b7d73935005ca1a297ef69ace89cb2e812d8dcd
Christophe Jollivet <jollivet@apside.fr>
2022-05-24 09:43:05 +0200

    test OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,5 @@ import { Form, NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'angularCRM';
-  fruit='apple';
-  myCssClass='apple';
-  names=['Bob', 'John', 'Paul']
-  car:string|undefined='renault'
 
-  validateForm(carForm:NgForm){
-    console.log(this.car);
-    console.log(carForm.value);
-
-  }
-
-  change($event:MouseEvent):void{
-    if(this.fruit==='apple'){
-      this.fruit='banana';
-      this.myCssClass='banana';
-    }else{
-      this.fruit='apple';
-      this.myCssClass='apple';
-    }
-    console.log($event);
-  }
 }
